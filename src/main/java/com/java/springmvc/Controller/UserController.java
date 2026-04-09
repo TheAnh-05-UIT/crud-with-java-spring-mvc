86af97da87a55fc7371dbfe566aa5a67f951a1c6
TheAnh-05-UIT <theeanh01.2005@gmail.com>
2026-04-10 00:11:13 +0700

    create form new user and The controller retrieves data from the view

--- src/main/java/com/java/springmvc/Controller/UserController.java
+++ src/main/java/com/java/springmvc/Controller/UserController.java
@@ -1,13 +1,42 @@
 package com.java.springmvc.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.java.springmvc.domain.User;
+import com.java.springmvc.service.UserService;
 
 @Controller
 public class UserController {
 
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @RequestMapping("/")
-    public String index() {
+    public String getHomePage(Model model) {
+        String test = this.userService.Program();
+        model.addAttribute("Hello", test);
+        return "Hello";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String getUserForm(
+            @ModelAttribute("newUser") User user,
+            Model model) {
+        System.out.println(user);
         return "Hello";
     }
 }
---
+++ src/main/java/com/java/springmvc/service/UserService.java
@@ -0,0 +1,11 @@
+package com.java.springmvc.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public String Program() {
+        return "hello service";
+    }
+}
