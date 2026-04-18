d5bb4b94b1b48a84855cf9a9eebc581fcfb2a3c6
TheAnh-05-UIT <theeanh01.2005@gmail.com>
2026-04-18 21:06:56 +0700

    Establish a one-to-many relationship between user and order

--- src/main/java/com/java/springmvc/domain/Order.java
+++ src/main/java/com/java/springmvc/domain/Order.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -15,6 +17,11 @@ public class Order {
     private Long id;
 
     private double totalPrice;
+
+    // order many to one user
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
     public Long getId() {
         return id;
