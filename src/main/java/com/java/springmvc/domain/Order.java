1e8805a0e2a0150a311781415b756b70299333d2
TheAnh-05-UIT <theeanh01.2005@gmail.com>
2026-04-18 21:43:53 +0700

    The one-to-many relationship between order and order_detail

--- src/main/java/com/java/springmvc/domain/Order.java
+++ src/main/java/com/java/springmvc/domain/Order.java
@@ -1,11 +1,14 @@
 package com.java.springmvc.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +25,10 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    // order one to many order_detail
+    @OneToMany(mappedBy = "order")
+    private List<orderDetail> orderDetails;
 
     public Long getId() {
         return id;
