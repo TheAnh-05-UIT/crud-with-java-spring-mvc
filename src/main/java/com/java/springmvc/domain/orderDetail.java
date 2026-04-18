9c36968cfc43c88a9007359fd83c78809322a83d
TheAnh-05-UIT <theeanh01.2005@gmail.com>
2026-04-18 21:31:08 +0700

    Add an order_detail object to simplify the relationship between order and product from many-to-many to two one-to-many relationships

---
+++ src/main/java/com/java/springmvc/domain/orderDetail.java
@@ -0,0 +1,49 @@
+package com.java.springmvc.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "order_detail")
+public class orderDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long quantity;
+    private double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "orderDetail [id=" + id + ", quantity=" + quantity + ", price=" + price + "]";
+    }
+
+}
