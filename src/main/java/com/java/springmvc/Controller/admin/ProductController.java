e4a5de56a496ce645bd39e42804485c11baae754
TheAnh-05-UIT <theeanh01.2005@gmail.com>
2026-04-25 17:37:20 +0700

    Create a new URL for the "Create a New Product" section

--- src/main/java/com/java/springmvc/Controller/admin/ProductController.java
+++ src/main/java/com/java/springmvc/Controller/admin/ProductController.java
@@ -1,13 +1,32 @@
 package com.java.springmvc.Controller.admin;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.java.springmvc.domain.Product;
 
 @Controller
 public class ProductController {
 
-    @RequestMapping("/admin/product")
-    public String getDashboardAdmin() {
+    @GetMapping("/admin/product")
+    public String getDashboardProduct() {
         return "/admin/product/view-product";
+    }
+
+    @GetMapping("/admin/product/create")
+    public String getCreateProductPage(
+            Model model) {
+        model.addAttribute("newProduct", new Product());
+        return "/admin/product/create-product";
+    }
+
+    @PostMapping("/admin/product/create")
+    public String getProductFrom(
+            @ModelAttribute("newProduct") Product product,
+            Model model) {
+        return "/admin/product/create-product";
     }
 }
