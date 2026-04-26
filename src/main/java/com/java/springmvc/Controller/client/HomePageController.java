8c703c9b4172500402d77a6babbaac05dc4f26b6
TheAnh-05-UIT <theeanh01.2005@gmail.com>
2026-04-26 23:47:03 +0700

    Render the product data to the user interface when the admin creates a new product

--- src/main/java/com/java/springmvc/Controller/client/HomePageController.java
+++ src/main/java/com/java/springmvc/Controller/client/HomePageController.java
@@ -1,13 +1,27 @@
 package com.java.springmvc.Controller.client;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.java.springmvc.domain.Product;
+import com.java.springmvc.service.ProductService;
 
 @Controller
 public class HomePageController {
 
+    private final ProductService productService;
+
+    public HomePageController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/")
-    public String gẹHomePage() {
+    public String gẹHomePage(Model model) {
+        List<Product> listProducts = this.productService.handleGetAllProduct();
+        model.addAttribute("products", listProducts);
         return "client/home/view-home";
     }
 }
