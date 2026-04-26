102cfaa2d0f057fda296305dd4c4df949d8322f3
TheAnh-05-UIT <theeanh01.2005@gmail.com>
2026-04-26 19:09:05 +0700

    ProductService connects to ProductRepository to query the database and update the product corresponding to the id

--- src/main/java/com/java/springmvc/service/ProductService.java
+++ src/main/java/com/java/springmvc/service/ProductService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.java.springmvc.domain.Product;
 import com.java.springmvc.repository.ProductRepository;
@@ -12,9 +13,12 @@ import com.java.springmvc.repository.ProductRepository;
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final UploadFileService uploadFileService;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository,
+            UploadFileService uploadFileService) {
         this.productRepository = productRepository;
+        this.uploadFileService = uploadFileService;
     }
 
     public List<Product> handleGetAllProduct() {
@@ -31,5 +35,25 @@ public class ProductService {
             return optionalProduct.get();
         }
         return null;
+    }
+
+    public Product handleUpdateProductById(Long id, Product productUpdate, MultipartFile file) {
+        Product updateProductById = this.handleGetProductById(id);
+        updateProductById.setName(productUpdate.getName());
+        updateProductById.setPrice(productUpdate.getPrice());
+        updateProductById.setDetailDesc(productUpdate.getDetailDesc());
+        updateProductById.setShortDesc(productUpdate.getShortDesc());
+        updateProductById.setQuantity(productUpdate.getQuantity());
+        updateProductById.setFactory(productUpdate.getFactory());
+        updateProductById.setTarget(productUpdate.getTarget());
+
+        String fileName = file.getOriginalFilename();
+        if (fileName != null && !fileName.equals("")) {
+            String updateFileName = this.uploadFileService.handleStorefile(file, "product");
+            if (!updateFileName.equals("")) {
+                updateProductById.setImage(updateFileName);
+            }
+        }
+        return this.productRepository.save(updateProductById);
     }
 }
