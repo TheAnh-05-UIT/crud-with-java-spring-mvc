9077aa7b00ec215208c197b122a5ad3a2355aa09
TheAnh-05-UIT <theeanh01.2005@gmail.com>
2026-04-27 04:43:54 +0700

    Create new URLs for the register and login forms and perform the conversion from RegisterDTO to User

--- src/main/java/com/java/springmvc/Controller/client/HomePageController.java
+++ src/main/java/com/java/springmvc/Controller/client/HomePageController.java
@@ -5,23 +5,51 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.java.springmvc.domain.Product;
+import com.java.springmvc.domain.User;
+import com.java.springmvc.domain.dto.RegisterDTO;
 import com.java.springmvc.service.ProductService;
+import com.java.springmvc.service.UserService;
 
 @Controller
 public class HomePageController {
 
     private final ProductService productService;
+    private final UserService userService;
 
-    public HomePageController(ProductService productService) {
+    public HomePageController(ProductService productService,
+            UserService userService) {
         this.productService = productService;
+        this.userService = userService;
     }
 
     @GetMapping("/")
-    public String gẹHomePage(Model model) {
+    public String getHomePage(Model model) {
         List<Product> listProducts = this.productService.handleGetAllProduct();
         model.addAttribute("products", listProducts);
         return "client/home/view-home";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage(Model model) {
+        model.addAttribute("register", new RegisterDTO());
+        return "client/auth/register";
+    }
+
+    @PostMapping("/register")
+    public String RegisterForm(Model model,
+            @ModelAttribute("register") RegisterDTO registerDTO) {
+        User user = this.userService.regiterDTOtoUser(registerDTO);
+        user.setRole(this.userService.handleGetRoleByName("USER"));
+        this.userService.handleCreateUser(user);
+        return "client/auth/register";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage(Model model) {
+        return "client/auth/login";
     }
 }
